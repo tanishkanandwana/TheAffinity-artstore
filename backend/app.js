9694883c78a5018ae84e4f6da3cf02898afaa62b
tanishka nandwana <nandwanatani6@gmail.com>
2025-08-12 23:35:51 +0530

    upadted CORS for frontend deploy

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 
 const allowedOrigins = [
- "https://mellifluous-tapioca-399521.netlify.app",
+ "https://subtle-cat-f0e9bd.netlify.app",
   "http://localhost:5173",
 ];
 
