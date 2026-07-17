73b4489a73f81ad7461e00377769f4f7991cc0b9
tanishka nandwana <nandwanatani6@gmail.com>
2026-07-17 21:46:56 +0530

    minor changes done

--- backend/models/CustomRequest.js
+++ backend/models/CustomRequest.js
@@ -5,7 +5,7 @@ const customRequestSchema = new mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User", // link to User model
+      ref: "user", // link to User model
       required: true,
     },
     artworkType: {
